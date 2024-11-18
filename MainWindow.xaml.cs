2dfbb19d64dd6a0e0005a865a5ca913500e6501a
Doeoll <82157115+Doeoll@users.noreply.github.com>
2024-11-18 12:27:07 +0900

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App1
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            double n1, n2;
+            double.TryParse(T1.Text, out n1);
+            double.TryParse(T2.Text, out n2);
+            double sum =n1 + n2;
+            T3.Text = sum.ToString();
+        }
+    }
+}
